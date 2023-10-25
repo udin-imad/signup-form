c96153906787e9e3c3a13dcafeb1655391e87826
udin-imad <udinwebdev@gmail.com>
2023-10-25 15:31:46 +0700

    revert

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const passwordConfirm = document.querySelector('#confirm_pwd');
 const errorMessage = document.getElementById('password-mismatch-message');
 const form = document.querySelector('form');
 const phone = document.querySelector('#phone');
+const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/
 
 form.addEventListener('submit', function (event) {
   if (password.value !== passwordConfirm.value) {
@@ -23,44 +24,4 @@ password.addEventListener('input', function () {
 passwordConfirm.addEventListener('input', function () {
   errorMessage.style.display = 'none';
   passwordConfirm.setCustomValidity(''); // Reset custom validity
-});
-
-password.addEventListener('invalid', function () {
-  this.setCustomValidity('Please enter a valid password with at least 8 characters, including one uppercase letter, one lowercase letter, and one number.')
-});
-
-passwordConfirm.addEventListener('invalid', function () {
-  this.setCustomValidity('Please confirm your password.')
-});
-
-firstName.addEventListener('invalid', function () {
-  this.setCustomValidity('Please enter your first name')
-});
-
-firstName.addEventListener('input', function () {
-  this.setCustomValidity('')
-});
-
-lastName.addEventListener('invalid', function () {
-  this.setCustomValidity('Please enter your last name')
-});
-
-lastName.addEventListener('input', function () {
-  this.setCustomValidity('')
-});
-
-email.addEventListener('invalid', function () {
-  this.setCustomValidity('Please enter valid email, example:email@domain.com')
-});
-
-email.addEventListener('input', function () {
-  this.setCustomValidity('')
-});
-
-phone.addEventListener('invalid', function () {
-  this.setCustomValidity('Please enter a valid phone number')
-});
-
-phone.addEventListener('input', function () {
-  this.setCustomValidity('')
 });
