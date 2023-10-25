c90ba71cd052c5bffbfd3e3caad9661304d6025f
udin-imad <udinwebdev@gmail.com>
2023-10-25 14:51:24 +0700

    set custom validation message

--- script.js
+++ script.js
@@ -1,24 +1,66 @@
+const firstName = document.querySelector('#first_name');
+const lastName = document.querySelector('#last_name');
+const email = document.querySelector('#email');
 const password = document.querySelector('#pwd');
 const passwordConfirm = document.querySelector('#confirm_pwd');
 const errorMessage = document.getElementById('password-mismatch-message');
 const form = document.querySelector('form');
+const phone = document.querySelector('#phone');
 
 form.addEventListener('submit', function(event) {
   if (password.value !== passwordConfirm.value) {
       errorMessage.style.display = 'block';
-      document.getElementById('confirm_pwd').setCustomValidity('Passwords do not match'); // Set custom validity
+      passwordConfirm.setCustomValidity('Passwords do not match'); // Set custom validity
       event.preventDefault(); // Prevent form submission
   }
 });
 
 password.addEventListener('input', function() {
-  const errorMessage = document.getElementById('password-mismatch-message');
   errorMessage.style.display = 'none';
-  document.getElementById('confirm_pwd').setCustomValidity(''); // Reset custom validity
+  passwordConfirm.setCustomValidity(''); // Reset custom validity
 });
 
 passwordConfirm.addEventListener('input', function() {
-  const errorMessage = document.getElementById('password-mismatch-message');
   errorMessage.style.display = 'none';
-  document.getElementById('confirm_pwd').setCustomValidity(''); // Reset custom validity
+  passwordConfirm.setCustomValidity(''); // Reset custom validity
 });
+
+password.addEventListener('invalid', function(){
+  this.setCustomValidity('Please enter a valid password with at least 8 characters, including one uppercase letter, one lowercase letter, and one number.')
+})
+
+passwordConfirm.addEventListener('invalid', function(){
+  this.setCustomValidity('Please confirm your password.')
+})
+
+firstName.addEventListener('invalid', function() {
+  this.setCustomValidity('Please enter your first name')
+})
+
+firstName.addEventListener('input', function() {
+  this.setCustomValidity('')
+})
+
+lastName.addEventListener('invalid', function() {
+  this.setCustomValidity('Please enter your last name')
+})
+
+lastName.addEventListener('input', function() {
+  this.setCustomValidity('')
+})
+
+email.addEventListener('invalid', function() {
+  this.setCustomValidity('Please enter valid email, example:email@domain.com')
+})
+
+email.addEventListener('input', function() {
+  this.setCustomValidity('')
+})
+
+phone.addEventListener('invalid', function() {
+  this.setCustomValidity('Please enter a valid phone number')
+})
+
+phone.addEventListener('input', function() {
+  this.setCustomValidity('')
+})
