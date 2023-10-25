355a4b902be6b67556af44738bb39ffcc8a65c4d
udin-imad <udinwebdev@gmail.com>
2023-10-25 14:23:16 +0700

    fix: user still can submit when pwd dont match

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 const password = document.querySelector('#pwd');
 const passwordConfirm = document.querySelector('#confirm_pwd');
-const error = document.querySelector('.error');
+const errorMessage = document.getElementById('password-mismatch-message');
+const form = document.querySelector('form');
 
-function confirmPassword() {
-    const textError = password.value === passwordConfirm.value ? '':'* password do not match'
-    error.textContent = textError
-}
+form.addEventListener('submit', function(event) {
+  if (password.value !== passwordConfirm.value) {
+      errorMessage.style.display = 'block';
+      document.getElementById('confirm_pwd').setCustomValidity('Passwords do not match'); // Set custom validity
+      event.preventDefault(); // Prevent form submission
+  }
+});
 
-passwordConfirm.addEventListener('change', confirmPassword);
+password.addEventListener('input', function() {
+  const errorMessage = document.getElementById('password-mismatch-message');
+  errorMessage.style.display = 'none';
+  document.getElementById('confirm_pwd').setCustomValidity(''); // Reset custom validity
+});
+
+passwordConfirm.addEventListener('input', function() {
+  const errorMessage = document.getElementById('password-mismatch-message');
+  errorMessage.style.display = 'none';
+  document.getElementById('confirm_pwd').setCustomValidity(''); // Reset custom validity
+});
